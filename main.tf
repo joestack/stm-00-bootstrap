9fe86473f3b9c3e7c0b83fd4a29fabbb9d8e1296
joestack <joern@joern-online.de>
2021-11-05 21:06:17 +0100

    foo9

--- main.tf
+++ main.tf
@@ -19,3 +19,25 @@ resource "null_resource" "foo" {
   # access_key_id = file("${path.root}/aws_access_key_id")
   # }
 }
+
+
+resource "null_resource" "bar" {
+  
+  depends_on = [
+    null_resource.foo
+  ]
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  # provisioner "local-exec" {
+  #   command = "echo $AWS_ACCESS_KEY_ID > ${path.root}/aws_access_key_id"
+  # }
+  
+   locals {
+  # #secret_access_key = var.aws_secret_access_key
+  # #session_token = var.aws_session_token
+   access_key_id = file("${path.root}/aws_access_key_id")
+   }
+}
