5152c9489e7b7e3dd5e1bc0e307a4fe66e716f00
joestack <joern@joern-online.de>
2021-11-05 20:38:50 +0100

    foo5

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "aws_access_key_id" {
   value = local.access_key_id
 }
 
-output "aws_secret_access_key" {
-  value = local.secret_access_key
-}
+# output "aws_secret_access_key" {
+#   value = local.secret_access_key
+# }
 
-output "aws_session_token" {
-  value = local.session_token
-}
+# output "aws_session_token" {
+#   value = local.session_token
+# }
 
