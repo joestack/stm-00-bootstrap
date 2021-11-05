c95d6bfb25e544e1d7a1f5a612a23bf753520e95
joestack <joern@joern-online.de>
2021-11-05 20:24:39 +0100

    foo3

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ terraform {
   required_version = ">= 0.12"
 }
 
-
+locals {
+  secret_access_key = var.aws_secret_access_key
+  session_token = var.aws_session_token
+  access_key_id = var.aws_access_key_id
+}
--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "aws_access_key_id" {
-  value = var.AWS_ACCESS_KEY_ID
+  value = local.access_key_id
 }
 
 output "aws_secret_access_key" {
-  value = var.aws_secret_access_key
+  value = local.secret_access_key
 }
 
 output "aws_session_token" {
-  value = var.aws_session_token
+  value = local.session_token
 }
 
