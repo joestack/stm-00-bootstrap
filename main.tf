32b5799a800f66e728c5ac03da68092c380a9fde
joestack <joern@joern-online.de>
2021-11-05 20:35:40 +0100

    foo4

--- main.tf
+++ main.tf
@@ -2,8 +2,21 @@ terraform {
   required_version = ">= 0.12"
 }
 
+resource "null_resource" "foo" {
+  
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "echo $AWS_ACCESS_KEY_ID > ${path.root}/aws_access_key_id"
+  }
+}
+
+
+
 locals {
-  secret_access_key = var.aws_secret_access_key
-  session_token = var.aws_session_token
-  access_key_id = var.aws_access_key_id
+  #secret_access_key = var.aws_secret_access_key
+  #session_token = var.aws_session_token
+  access_key_id = file("${path.root}/aws_access_key_id")
 }
