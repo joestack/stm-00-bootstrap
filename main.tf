246a170d839f82438579d7d4c4b747c5ec0d840c
joestack <joern@joern-online.de>
2021-11-06 12:48:45 +0100

    bar 2

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    tfe = {
+      version = "~> 0.26.0"
+    }
+  }
 }
+
+provider "tfe" {
+  #hostname = var.hostname
+  token    = var.tfc_token
+  #version  = "~> 0.26.0"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "stm-10-foundation"
+  organization = "joestack"
+  #tag_names    = ["test", "app"]
+}
+
 
 variable "tfc_token" {}
 
@@ -8,3 +26,5 @@ output "tfc_token" {
   value = var.tfc_token
   sensitive = true
 }
+
+
