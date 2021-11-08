41874d339bdd6b91caff0323c2818c5151bab752
joestack <joern@joern-online.de>
2021-11-08 16:00:20 +0100

    bar 9

--- main.tf
+++ main.tf
@@ -31,6 +31,26 @@ resource "github_repository" "stm-10-foundation" {
 
 }
 
+resource "github_repository" "stm-20-bastion" {
+  name        = "stm-20-bastion"
+  description = "My programmatic generated page"
+ 
+  visibility = "public"
+
+  auto_init = true
+
+}
+
+resource "github_repository" "stm-30-ptfe" {
+  name        = "stm-30-ptfe"
+  description = "My programmatic generated page"
+ 
+  visibility = "public"
+
+  auto_init = true
+
+}
+
 resource "tfe_workspace" "stm-10-foundation" {
   name         = "stm-10-foundation"
   organization = "joestack"
@@ -39,5 +59,39 @@ resource "tfe_workspace" "stm-10-foundation" {
     identifier = github_repository.stm-10-foundation.full_name
     oauth_token_id = var.oauth_token_id
   }
+  allow_destroy_plan = true
+  auto_apply = true
+  global_remote_state = true 
+  queue_all_runs = true 
+  terraform_version = "1.0.10" 
 }
 
+resource "tfe_workspace" "stm-20-bastion" {
+  name         = "stm-20-bastion"
+  organization = "joestack"
+  #tag_names    = ["test", "app"]
+  vcs_repo {
+    identifier = github_repository.stm-20-bastion.full_name
+    oauth_token_id = var.oauth_token_id
+  }
+  allow_destroy_plan = true
+  auto_apply = true
+  global_remote_state = true 
+  queue_all_runs = true 
+  terraform_version = "1.0.10"
+}
+
+resource "tfe_workspace" "stm-30-ptfe" {
+  name         = "stm-30-ptfe"
+  organization = "joestack"
+  #tag_names    = ["test", "app"]
+  vcs_repo {
+    identifier = github_repository.stm-30-ptfe.full_name
+    oauth_token_id = var.oauth_token_id
+  }
+  allow_destroy_plan = true
+  auto_apply = true
+  global_remote_state = true 
+  queue_all_runs = true 
+  terraform_version = "1.0.10"
+}
