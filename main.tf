e87e44d62971943ba23052730db0d3b93ce1641e
joestack <joern@joern-online.de>
2021-11-10 17:35:00 +0100

    restructure 2

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    tfe = {
+      version = "~> 0.26.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname = var.tfc_host
+  token    = var.tfc_token
+  #version  = "~> 0.26.0"
+}
+
+provider "github" {
+  token = var.github_token
+}
+
+
+
 ### Creating GitHub Repos ###
 
 resource "github_repository" "stm-10-foundation" {
